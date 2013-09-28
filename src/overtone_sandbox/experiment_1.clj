a637381f293d60d585c928193d7990fcb9a7542a
Wojtek Majewski <jumski@gmail.com>
2013-09-28 19:45:34 +0200

    wip

---
+++ src/overtone_sandbox/experiment_1.clj
@@ -0,0 +1,16 @@
+(ns overtone-sandbox.experiment_1 (:import (java.io File))
+
+;; (def samples
+;;   (file-seq (clojure.java.io/File. "samples/amen-break/")))
+;;
+;; (defn filename-to-samplename [filename]
+;;   (last (last
+;;           (re-seq #"([a-z0-9]{5}_VEXST_)([\w+_0-9]+)\.wav" filename))))
+;;
+;; (defn samplename-to-samplekey [samplename]
+;;   (keyword samplename))
+;;
+;;
+;;
+;; (filename-to-samplename "26899_VEXST_Semi_Snare_4.wav")
+
---
+++ test/overtone_sandbox/core_test.clj
@@ -0,0 +1,7 @@
+(ns overtone-sandbox.core-test
+  (:require [clojure.test :refer :all]
+            [overtone-sandbox.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
