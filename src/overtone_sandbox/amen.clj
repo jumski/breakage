8a641de6e4a1b244abb8bc9dc62d128eb27996e4
Wojtek Majewski <jumski@gmail.com>
2013-10-12 12:50:25 +0200

    wip

--- src/overtone_sandbox/amen.clj
+++ src/overtone_sandbox/amen.clj
@@ -1,24 +1,26 @@
 (ns overtone-sandbox.amen
   (:require [overtone-sandbox.kit :as kit]))
 
+(def kit-amen (kit/load-kit "samples/amen-break"))
+
 (defn hax-loop []
-  (for [sample samples] (do
-                          (sample)
+  (for [hit (vals kit-amen)] (do
+                          (hit)
                           (Thread/sleep 85))))
 
 
 ;;;;;;;;;;;; stolen from overtone wiki
-(defn looper [nome sound]
-    (let [beat (nome)]
-        (at (nome beat) (sound))
-        (apply-at (nome (inc beat)) looper nome sound [])))
-
-(defn song [tempo]
-  (let [kick-nome (metronome tempo)
-        hat-nome (metronome (/ tempo 2))]
-    (do
-      (looper hat-nome (:chat1 kit))
-      (looper kick-nome (:kick1 kit)))))
+;; (defn looper [nome sound]
+;;     (let [beat (nome)]
+;;         (at (nome beat) (sound))
+;;         (apply-at (nome (inc beat)) looper nome sound [])))
+;;
+;; (defn song [tempo]
+;;   (let [kick-nome (metronome tempo)
+;;         hat-nome (metronome (/ tempo 2))]
+;;     (do
+;;       (looper hat-nome (:chat1 kit))
+;;       (looper kick-nome (:kick1 kit)))))
 
 ;; (hax-loop)
 
