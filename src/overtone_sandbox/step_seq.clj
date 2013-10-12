43b50aedc108eadd4ffd22d2b077c783fbaf1196
Wojtek Majewski <jumski@gmail.com>
2013-10-12 15:12:08 +0200

    wip

--- src/overtone_sandbox/step_seq.clj
+++ src/overtone_sandbox/step_seq.clj
@@ -12,24 +12,23 @@
 (def h 1)
 (def c 1)
               ;;  [X . . . X . . . X . . . X . . .]
-(def pat {:kick1  [k _ _ _ _ _ _ k _ _ k _ _ _ _ _]
+(def pattern {:kick1  [k _ _ _ _ _ _ k _ _ k _ _ _ _ _]
           :snare1 [_ _ _ _ s _ _ _ _ _ _ _ s _ _ _]
           :chat1  [_ _ h _ _ _ h _ h _ _ _ _ _ h _]
           :csnare [_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _]})
 
 (def metro (metronome 194))
 
-(defn play-pat [beat i]
+(defn play-pattern [beat hitname]
   (let [t (mod beat 4)
-        p (vec (take 4 (drop (* 4 t) (pat i))))]
-    ;; (println (str t p))))
-    (if (= 1 (p 0)) (at (metro (+ 0.00 beat)) ((amen i))))
-    (if (= 1 (p 1)) (at (metro (+ 0.25 beat)) ((amen i))))
-    (if (= 1 (p 2)) (at (metro (+ 0.50 beat)) ((amen i))))
-    (if (= 1 (p 3)) (at (metro (+ 0.75 beat)) ((amen i))))))
+        p (vec (take 4 (drop (* 4 t) (pattern hitname))))]
+    (if (= 1 (p 0)) (at (metro (+ 0.00 beat)) ((amen hitname))))
+    (if (= 1 (p 1)) (at (metro (+ 0.25 beat)) ((amen hitname))))
+    (if (= 1 (p 2)) (at (metro (+ 0.50 beat)) ((amen hitname))))
+    (if (= 1 (p 3)) (at (metro (+ 0.75 beat)) ((amen hitname))))))
 
 (defn player [beat]
-  (doseq [i (keys pat)] (play-pat beat i))
+  (doseq [hitname (keys pattern)] (play-pattern beat hitname))
   (apply-at (metro (inc beat)) #'player (inc beat) []))
 
 (stop)
