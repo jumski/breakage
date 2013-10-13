7fa4ae8179c78e799e34112a71ae360915b09125
Wojtek Majewski <jumski@gmail.com>
2013-10-13 16:16:02 +0200

    wip

--- src/jumski/breakage/break_1.clj
+++ src/jumski/breakage/break_1.clj
@@ -5,27 +5,27 @@
 
 (def amen-break (kit/load-kit "samples/amen-break"))
 
-(def dnb-base {:kick1    [9 _ _ _ _ _ _ 9 _ _ 9 _ _ _ _ _]
+(def dnb-base {:kick1    [7 _ _ _ _ _ _ 7 _ _ 7 _ _ _ _ _]
                :snare2   [_ _ _ _ 7 _ _ _ _ _ _ _ 7 _ _ _]
                :chat2    [_ _ 3 _ _ _ 4 _ 4 _ _ _ _ _ 3 _]
                :ssnare3  [_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _]})
 
-(def dnb-base2 {:kick1   [9 _ _ _ _ _ _ _ _ _ 9 _ _ _ _ _]
+(def dnb-base2 {:kick1   [7 _ _ _ _ _ _ _ _ _ 7 _ _ _ _ _]
                 :snare2  [_ _ _ _ 7 _ _ _ _ _ _ _ 7 _ _ _]
                 :chat2   [_ _ 3 _ _ _ 4 _ 4 _ _ _ _ _ 3 _]
                 :ssnare3 [_ _ _ _ _ _ _ _ _ 6 _ _ _ _ _ _]})
 
-(def dnb-final {:kick1   [9 _ 9 _ _ _ _ _ _ _ 9 _ _ _ _ _]
+(def dnb-final {:kick1   [7 _ 7 _ _ _ _ _ _ _ 7 _ _ _ _ _]
                 :snare2  [_ _ _ _ 7 _ _ _ _ _ _ _ 7 _ _ _]
                 :chat2   [_ _ 3 _ _ _ 4 _ 4 _ _ _ _ _ 3 _]
                 :ssnare3 [_ _ _ _ _ _ _ 6 _ _ _ _ _ _ _ _]})
 
-(def dnb-final {:kick1   [9 _ 9 _ _ _ _ _ _ _ 9 _ _ _ _ _]
+(def dnb-final {:kick1   [7 _ 7 _ _ _ _ _ _ _ 7 _ _ _ _ _]
                 :snare2  [_ _ _ _ 7 _ _ _ _ _ _ _ 7 _ _ _]
                 :chat2   [_ _ 3 _ _ _ 4 _ 4 _ _ _ _ _ 3 _]
                 :ssnare3 [_ _ _ _ _ _ _ 6 _ 6 _ _ _ _ _ _]})
 
-(def dnb-final2 {:kick1   [9 _ 9 _ _ _ _ _ _ _ _ _ _ _ _ _]
+(def dnb-final2 {:kick1   [7 _ 7 _ _ _ _ _ _ _ _ _ _ _ _ _]
                  :snare2  [_ _ _ _ 7 _ _ _ _ _ _ _ _ _ _ _]
                  :chat2   [_ _ 3 _ _ _ 4 _ 4 _ 4 _ 4 _ 4 _]
                  :ssnare3 [_ _ _ _ _ _ _ 6 _ 6 _ 6 _ 6 _ 6]})
