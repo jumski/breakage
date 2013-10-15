b7cdf356817415e29f42b611ef3f5ca8b66e3282
Wojtek Majewski <jumski@gmail.com>
2013-10-16 01:08:08 +0200

    refactor

--- src/jumski/breakage/breaks/break_1.clj
+++ src/jumski/breakage/breaks/break_1.clj
@@ -1,38 +1,41 @@
 (ns jumski.breakage.breaks.break_1
-  (:use [jumski.breakage.step-seq])
+  (:use [jumski.breakage.step-seq]
+        [jumski.breakage.pattern])
   (:require [jumski.breakage.kit :as kit])
   (:require [overtone.live :as o]))
 
 (def amen-break (kit/load-kit "samples/amen-break"))
 
-(def dnb-base {:kick1     [7 _ _ _ _ _ _ 7 _ _ 7 _ _ _ _ _]
-               :snare2    [_ _ _ _ 7 _ _ _ _ _ _ _ 7 _ _ _]
-               :chat2     [_ _ 3 _ _ _ 4 _ 4 _ _ _ _ _ 3 _]
-               :ssnare3   [_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _]})
+(def dnb [:kick1     [7 _ _ _ _ _ _ 7 _ _ 7 _ _ _ _ _]
+          :snare2    [_ _ _ _ 7 _ _ _ _ _ _ _ 7 _ _ _]
+          :chat2     [_ _ 3 _ _ _ 4 _ 4 _ _ _ _ _ 3 _]
+          :ssnare3   [_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _]])
 
-(def dnb-base2 {:kick1    [7 _ _ _ _ _ _ _ _ _ 7 _ _ _ _ _]
-                :snare2   [_ _ _ _ 7 _ _ _ _ _ _ _ 7 _ _ _]
-                :chat2    [_ _ 3 _ _ _ 4 _ 4 _ _ _ _ _ 3 _]
-                :ssnare3  [_ _ _ _ _ _ _ _ _ 6 _ _ _ _ _ _]})
+(def dnb  [:kick1    [7 _ _ _ _ _ _ _ _ _ 7 _ _ _ _ _]
+           :snare2   [_ _ _ _ 7 _ _ _ _ _ _ _ 7 _ _ _]
+           :chat2    [_ _ 3 _ _ _ 4 _ 4 _ _ _ _ _ 3 _]
+           :ssnare3  [_ _ _ _ _ _ _ _ _ 6 _ _ _ _ _ _]])
 
-(def dnb-final {:kick1    [7 _ 7 _ _ _ _ _ _ _ 7 _ _ _ _ _]
-                :snare2   [_ _ _ _ 7 _ _ _ _ _ _ _ 7 _ _ _]
-                :chat2    [_ _ 3 _ _ _ 4 _ 4 _ _ _ _ _ 3 _]
-                :ssnare3  [_ _ _ _ _ _ _ 6 _ _ _ _ _ _ _ _]})
+(def dnb  [:kick1    [7 _ 7 _ _ _ _ _ _ _ 7 _ _ _ _ _]
+           :snare2   [_ _ _ _ 7 _ _ _ _ _ _ _ 7 _ _ _]
+           :chat2    [_ _ 3 _ _ _ 4 _ 4 _ _ _ _ _ 3 _]
+           :ssnare3  [_ _ _ _ _ _ _ 6 _ _ _ _ _ _ _ _]])
 
-(def dnb-final {:kick1    [7 _ 7 _ _ _ _ _ _ _ 7 _ _ _ _ _]
-                :snare2   [_ _ _ _ 7 _ _ _ _ _ _ _ 7 _ _ _]
-                :chat2    [_ _ 3 _ _ _ 4 _ 4 _ _ _ _ _ 3 _]
-                :ssnare3  [_ _ _ _ _ _ _ 6 _ 6 _ _ _ _ _ _]})
+(def dnb  [:kick1    [7 _ 7 _ _ _ _ _ _ _ 7 _ _ _ _ _]
+           :snare2   [_ _ _ _ 7 _ _ _ _ _ _ _ 7 _ _ _]
+           :chat2    [_ _ 3 _ _ _ 4 _ 4 _ _ _ _ _ 3 _]
+           :ssnare3  [_ 3 _ 2 _ 2 _ 6 _ 6 _ 2 _ 4 _ 3]])
 
-(def dnb-final2 {:kick1   [7 _ 7 _ _ _ _ _ 8 _ _ _ _ _ _ _]
-                 :snare2  [_ _ _ _ 7 _ _ _ _ _ _ _ 7 _ _ _]
-                 :chat2   [_ _ 3 _ _ _ 4 _ _ _ 4 _ 4 _ 4 _]
-                 :ssnare3 [_ _ _ _ _ _ _ 6 _ 6 _ 6 _ 6 _ 6]})
+(def dnb  [:kick1   [7 _ 7 _ _ _ _ _ 8 _ _ _ _ _ _ _]
+           :snare2  [_ _ _ _ 7 _ _ _ _ _ _ _ 7 _ _ _]
+           :chat2   [_ _ 3 _ _ _ 4 _ _ _ 4 _ 4 _ 4 _]
+           :ssnare3 [_ _ _ _ _ _ _ 4 _ 4 _ 4 _ 4 _ 4]
+                    [_ _ _ _ _ _ _ 1 _ 1 _ 1 _ 2 _ 1]])
 
-(defn pattern [] (merge-with concat
-                         dnb-base dnb-base2 dnb-base dnb-final
-                         dnb-base dnb-base2 dnb-base dnb-final2))
+(defn pattern [] dnb)
+;; (defn pattern [] (merge-with concat
+;;                          dnb-base dnb-base2 dnb-base dnb-final
+;;                          dnb-base dnb-base2 dnb-base dnb-final2))
 
 (o/stop)
 (play amen-break #'pattern 194)
