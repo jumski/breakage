44d089f30b60a151478405022db675217d6393a6
Wojtek Majewski <jumski@gmail.com>
2013-10-15 20:36:11 +0200

    pattern/split-on-keyword

--- src/jumski/breakage/pattern.clj
+++ src/jumski/breakage/pattern.clj
@@ -2,17 +2,24 @@
 
 (def _ (symbol "_"))
 
-(defn normalize [pat]
+(defn normalize [patt]
   (cond
-    (= 3 (count pat))
-      (let [vollen (count (pat 1))
-            pitlen (count (pat 2))
-            mislen (- vollen pitlen)]
-        (if (zero? mislen) pat
-          (let [tail (repeat mislen _)
-                pit (concat (pat 2) tail)]
-            (assoc pat 2 pit))))
-    (= 2 (count pat))
-      (let [len (count (last pat))
+    (= 3 (count patt))
+      (let [vlen (count (patt 1))
+            plen (count (patt 2))
+            missing (- vlen plen)]
+        (if (zero? missing) patt
+          (let [tail (repeat missing _)
+                pit (concat (patt 2) tail)]
+            (assoc patt 2 pit))))
+    (= 2 (count patt))
+      (let [len (count (last patt))
             pit (vec (repeat len _))]
-        (conj pat pit))))
+        (conj patt pit))))
+
+(defn split-on-keyword [patt]
+  (->> patt
+       (partition-by keyword?)
+       (partition 2)
+       concat
+       (map #(apply concat %))))
--- test/jumski/breakage/pattern_test.clj
+++ test/jumski/breakage/pattern_test.clj
@@ -10,5 +10,9 @@
     (p/normalize [:kick [1 2]]) => [:kick [1 2] [p/_ p/_]])
 
   (fact "appends missing pitches if count not match"
-    (p/normalize [:kick [1 2] [3]]) => [:kick [1 2] [3 p/_]])
-)
+    (p/normalize [:kick [1 2] [3]]) => [:kick [1 2] [3 p/_]]))
+
+(facts "about `split-on-keyword`"
+  (let [patt [:k 1 2 :v 3]]
+    (fact "split in proper place"
+      (p/split-on-keyword patt) => [[:k 1 2] [:v 3]])))
