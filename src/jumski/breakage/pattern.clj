3719a40c7672067e48870e6a8dac665d0d33450c
Wojtek Majewski <jumski@gmail.com>
2013-10-15 21:10:53 +0200

    vol and pit should be in vector

--- src/jumski/breakage/pattern.clj
+++ src/jumski/breakage/pattern.clj
@@ -3,19 +3,20 @@
 (def _ nil)
 
 (defn normalize [patt]
-  (cond
-    (= 3 (count patt))
-      (let [vlen (count (patt 1))
-            plen (count (patt 2))
-            missing (- vlen plen)]
-        (if (zero? missing) patt
-          (let [tail (repeat missing _)
-                pit (concat (patt 2) tail)]
-            (assoc patt 2 pit))))
-    (= 2 (count patt))
-      (let [len (count (last patt))
-            pit (vec (repeat len _))]
-        (conj patt pit))))
+  (let [flat (cond
+          (= 3 (count patt))
+          (let [vlen (count (patt 1))
+                plen (count (patt 2))
+                missing (- vlen plen)]
+            (if (zero? missing) patt
+              (let [tail (repeat missing _)
+                    pit (concat (patt 2) tail)]
+                (assoc patt 2 pit))))
+          (= 2 (count patt))
+          (let [len (count (last patt))
+                pit (vec (repeat len _))]
+            (conj patt pit)))]
+    {(first flat) (vec (rest flat))}))
 
 (defn split-on-keyword [patt]
   (->> patt
--- test/jumski/breakage/pattern_test.clj
+++ test/jumski/breakage/pattern_test.clj
@@ -11,13 +11,13 @@
 
 (facts "about `normalize`"
   (fact "returns as is if smp, vol and pit present"
-    (p/normalize [:kick [1 2] [3 4]]) => [:kick [1 2] [3 4]])
+    (p/normalize [:kick [1 2] [3 4]]) => {:kick [[1 2] [3 4]]})
 
   (fact "appends blank pitches if missing"
-    (p/normalize [:kick [1 2]]) => [:kick [1 2] [p/_ p/_]])
+    (p/normalize [:kick [1 2]]) => {:kick [[1 2] [p/_ p/_]]})
 
   (fact "appends missing pitches if count not match"
-    (p/normalize [:kick [1 2] [3]]) => [:kick [1 2] [3 p/_]]))
+    (p/normalize [:kick [1 2] [3]]) => {:kick [[1 2] [3 p/_]]}))
 
 ;; (facts "about `slice-for-beat`"
 ;;   (let [patt [[:kick (vec (range 0 16))]
