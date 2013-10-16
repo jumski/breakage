866cc2409fdbd7822f9d05ce6272635d1a5d9e0b
Wojtek Majewski <jumski@gmail.com>
2013-10-16 21:13:27 +0200

    wip

--- src/jumski/breakage/breaks/pitches_1.clj
+++ src/jumski/breakage/breaks/pitches_1.clj
@@ -6,23 +6,22 @@
 
 (def amen-break (kit/load-kit "samples/amen-break"))
 
-                     ;[X _ _ _ X _ _ _ X _ _ _ X _ _ _]
 (def base3 [
-            :kick1    [7 _ _ _ _ _ 7 _ _ _ _ _ _ _ _ _]
+           ;[X _ _ _ X _ _ _ X _ _ _ X _ _ _]
+  :kick1    [7 _ _ _ _ _ 7 _ _ _ _ _ _ _ _ _]
+  :chat1    [_ _ 4 _ 4 _ 4 _ 4 _ 4 _ _ _ _ _]
+  :stab     [_ _ _ _ _ _ _ _ _ _ 2 _ _ _ _ _]
 
-            :chat1    [_ _ 4 _ 4 _ 4 _ 4 _ 4 _ _ _ _ _]
+  :ssnare1  [_ _ _ 4 _ 2 _ 2 _ 4 _ 2 _ _ 6 _]
+            [_ _ _ 1 _ 2 _ 2 _ 1 _ 2 _ _ _ _]
 
-            :stab     [_ _ _ _ _ _ _ _ _ _ 2 _ _ _ _ _]
-
-            :ssnare1  [_ _ _ 4 _ 2 _ 2 _ 4 _ 2 _ _ 6 _]
-                      [_ _ _ 1 _ 2 _ 2 _ 1 _ 2 _ _ _ _]
-
-            :snare4   [_ _ _ _ _ _ _ _ _ _ _ _ 2 1 _ 1]
-                      [_ _ _ _ _ _ _ _ _ _ _ _ 4 _ _ _]
+  :snare4   [_ _ _ _ _ _ _ _ _ _ _ _ 2 1 _ 1]
+            [_ _ _ _ _ _ _ _ _ _ _ _ 4 _ _ _]
             ])
 
 (defn pattern [] base3)
 
 (o/stop)
+(o/volume 0.3)
 (play amen-break #'pattern 194)
 ;; (play-pattern amen-break pattern :snare1 0)
