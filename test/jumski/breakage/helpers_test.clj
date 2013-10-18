61b525136108169cb37286b16b5748c51092e0c9
Wojtek Majewski <jumski@gmail.com>
2013-10-18 20:31:16 +0200

    Wip

--- test/jumski/breakage/helpers_test.clj
+++ test/jumski/breakage/helpers_test.clj
@@ -2,18 +2,14 @@
 (use 'clojure.test)
 (require '[jumski.breakage.helpers :as h] :reload)
 
-(def full-pattern
-  [:kick [1 _ _ _]
-         [1 _ _ _]
-   :hat  [1 _ 1 _]])
-
 (deftest nil-placeholder
   (is (nil? h/_)))
 
 (deftest make-beat
   (deftest appends-empty-pitches
     (let [b (h/make-beat '(:k (1 k/_ k/_ k/_)))]
-      (is (= '(1 k/_ k/_ k/_) (-> b :k :volumes)))
-      (is (= '(nil nil nil nil) (-> b :k :pitches))))))
+      (and (is (= '(1 k/_ k/_ k/_) (-> b :k :volumes)))
+           (is (= '(nil nil nil nil) (-> b :k :pitches)))))))
 
+(require '[jumski.breakage.helpers :as h] :reload)
 (run-tests 'jumski.breakage.helpers-test)
