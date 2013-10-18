32067db1a090ed8c0879bcaf6eec429821a33b77
Wojtek Majewski <jumski@gmail.com>
2013-10-18 23:56:24 +0200

    WORX!!

--- src/jumski/breakage/helpers.clj
+++ src/jumski/breakage/helpers.clj
@@ -15,23 +15,21 @@
   (let [aseq (if (nil? aseq) [] aseq)]
     (->> (repeat _) (concat aseq) (take len))))
 
-(defn make-same-length [seqs]
-  (let [seqs (map sequence seqs)
-        cnts (map count seqs)
-        maxlen (apply max cnts)]
-    (map (partial fill-with-blanks maxlen) seqs)))
+(defn fixed-length [len seqs]
+  (map (partial fill-with-blanks len) seqs))
 
-(defn maxlength [aseq]
+(defn find-maxlength [aseq]
   (->>
-    (for [vp aseq] (map count vp))
+    (for [[_ & vp] aseq] (map count vp))
     flatten
     (apply max)))
 
 (defn make-beat [pat]
   (apply hash-map (mapcat concat
-  (let [splitted (split-on-keyword pat)]
+  (let [splitted (split-on-keyword pat)
+        maxlength (find-maxlength splitted)]
     (for [[hitname vol pit] splitted]
-      (let [[vol pit] (make-same-length [vol pit])]
+      (let [[vol pit] (fixed-length maxlength [vol pit])]
         [hitname {:volumes vol :pitches pit}]))))))
 
 
