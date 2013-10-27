53659f6638d392fcce4a9646dc1c2d5dd40b007e
Wojtek Majewski <jumski@gmail.com>
2013-10-27 20:15:49 +0100

    Small v3 improvements

--- src/jumski/breakage/breaks/v3.clj
+++ src/jumski/breakage/breaks/v3.clj
@@ -1,4 +1,4 @@
-(ns jumski.breakage.breaks.v2
+(ns jumski.breakage.breaks.v3
   (:use [jumski.breakage.sequencer] :reload)
   (:require [jumski.breakage.kit :as kit])
   (:require [overtone.live :as o]))
@@ -8,11 +8,11 @@
 (def base [
            :snare3 (->> [2 _ _] cycle (take 16))
                    (->> 10 (iterate #(* 0.9 %)) (take 16))
-           :kick2   [4 _ _ 4 _ _ _ _]
+           :kick3   [3 _ _ 3 _ _ _ _]
            :ssnare2 [_ _ _ _ _ _ 6 _]
            ;; :snare1  [_ _ _ 3 2 _ 2   _ _ 2 _ 3 2 _ 2   _]
            ;;          [_ _ _ _ 2 _ 0.6 _ 5 _ _ _ 2 _ 0.6 _]
-           :chat1   [2 _ _]
+           :chat2   [1 _ _]
                     (->> 1 (iterate #(* 0.9 %)) (take 8))
            ;; :chat2   [_ 1.5]
            ;; :ohat    [_ _ 1 _]
