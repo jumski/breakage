d85cf30e81c4c1576030ef156a47dd447de159dc
Wojtek Majewski <jumski@gmail.com>
2015-05-08 08:54:57 +0200

    Update test

--- test/jumski/breakage/mindstorm_test.clj
+++ test/jumski/breakage/mindstorm_test.clj
@@ -5,19 +5,19 @@
 (use 'midje.repl)
 (require '[jumski.breakage.mindstorm :as m] :reload)
 
-(facts "make-pattern"
+(facts "defpattern"
   (fact "Handles even length of tracks"
-    (m/make-pattern [:k 1 m/. 2 m/.
-                    :p 3 m/. 4 m/.]) => {:k {0 1, 2 2}
-                                        :p {0 3, 2 4}})
+        (do
+          (m/defpattern :intro
+            :k 1 m/. 2 m/.
+            :p 3 m/. 4 m/.)
+          (:intro @m/patterns) => {:k {0 1, 2 2}
+                                   :p {0 3, 2 4}}))
 
   (fact "Handles different lenght of tracks"
-    (m/make-pattern [:k 1 m/. 2
-                    :p 3 m/. 4 m/.]) => {:k {0 1 2 2}
-                                          :p {0 3, 2 4}}))
-
-(facts "defpattern"
-  (fact "parses stuff using make-pattern and stores in an atom"
-    (do
-      (m/defpattern :intro :k 1 m/. 2 m/.)
-      (:intro @m/patterns)) => {:k {0 1, 2 2}}))
+        (do
+          (m/defpattern
+            :k 1 m/. 2
+            :p 3 m/. 4 m/.)
+          (:intro @m/patterns) => {:k {0 1 2 2}
+                                   :p {0 3, 2 4}})))
