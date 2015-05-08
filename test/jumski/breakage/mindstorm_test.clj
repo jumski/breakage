556866b3b3d27bfe4bb0aa6544b7f3f5666221a9
Wojtek Majewski <jumski@gmail.com>
2015-05-08 09:08:16 +0200

    Improve spec

--- test/jumski/breakage/mindstorm_test.clj
+++ test/jumski/breakage/mindstorm_test.clj
@@ -5,19 +5,31 @@
 (use 'midje.repl)
 (require '[jumski.breakage.mindstorm :as m] :reload)
 
-(facts "defpattern"
-  (fact "Handles even length of tracks"
-        (do
-          (m/defpattern :intro
-            :k 1 m/. 2 m/.
-            :p 3 m/. 4 m/.)
-          (:intro @m/patterns) => {:k {0 1, 2 2}
-                                   :p {0 3, 2 4}}))
+(facts
+  "defpattern"
 
-  (fact "Handles different lenght of tracks"
-        (do
-          (m/defpattern
-            :k 1 m/. 2
-            :p 3 m/. 4 m/.)
-          (:intro @m/patterns) => {:k {0 1 2 2}
-                                   :p {0 3, 2 4}})))
+  (fact
+    "Handles even length of tracks"
+    (do
+      (m/defpattern :intro :k 1 2 :p 3 4)
+      (:intro @m/patterns) => {:k {0 1, 1 2}
+                               :p {0 3, 1 4}}))
+
+  (fact
+    "Handles different lenght of tracks"
+    (do
+      (m/defpattern :intro :k 1 2 :p 3 4 5)
+      (:intro @m/patterns) => {:k {0 1, 1 2}
+                               :p {0 3, 1 4, 2 5}}))
+
+  (fact
+    "Skips nil steps"
+    (do
+      (m/defpattern :intro :k 1 nil 2 nil)
+      (:intro @m/patterns)) => {:k {0 1, 2 2}})
+
+  (fact
+    "Anything other than integer is treated as nil step"
+    (do
+      (m/defpattern :intro :k 1 / + . 1 | = -)
+      (:intro @m/patterns)) => {:k {0 1, 4 1}}))
