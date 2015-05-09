b31c49a31866e9a34b9e63472508aa437c051a92
Wojtek Majewski <jumski@gmail.com>
2015-05-09 17:32:00 +0200

    Update overtone to 0.9.1 and clojure to 1.6.0

--- project.clj
+++ project.clj
@@ -3,9 +3,8 @@
   :url "http://github.com/jumski/breakage"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojars.runa/conjure "2.1.3"]
-                 [overtone "0.8.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [overtone "0.9.1"]
                  [org.clojure/math.numeric-tower "0.0.4"]]
   :profiles {:dev {:dependencies [[midje "1.6-beta1"]]}})
 
