ade104f5c8bb42a38c9fa7406608fbcd107d3d8c
Wojtek Majewski <jumski@gmail.com>
2015-05-14 01:55:25 +0200

    Proper sequencer working

--- src/jumski/breakage/tests/sequencer.clj
+++ src/jumski/breakage/tests/sequencer.clj
@@ -12,10 +12,10 @@
     (def . nil)
     (def sink (midi-out "USB"))
     (def atat-pool (mk-pool))
-    (def notemap {:kick1 60 :snare1 65 :chat1 66})
+    (def notemap {:kick1 60 :snare1 64 :chat1 66})
     (def patt (make-pattern [:kick1  9 . . 9 . . 9 .
                             :snare1 . . 4 . . 4 . .
-                            :chat1  . 2 . 2 . 2 . 2]))
+                            :chat1  2 4 2 4 2 4 2 4]))
 
     (def sequencer (atom nil))
 
@@ -25,7 +25,7 @@
 
     (def current-step (atom 0))
 
-    (defn play-current-step [patt notemap sink]
+    (defn play-and-advance [patt notemap sink]
       (do
         (doseq [[name steps] patt
                 :let [note (notemap name)
@@ -34,19 +34,22 @@
           (midi-note sink note velo 50))
         (swap! current-step inc)))
 
-    (doseq [step (range 64)]
-      (do
-        (play-current-step patt notemap sink)
-        (Thread/sleep 150)))
+    (future
+      (doseq [step (range 64)]
+        (do
+          (play-and-advance patt notemap sink)
+          (Thread/sleep (beat-ms 1/4 150)))))
 
-    (defn sequence [patt bpm notemap sink]
+    (defn play-pattern! [patt bpm notemap sink]
       "Starts sequencing midi messages to sink for pattern at given bpm.
       Uses notemap to translate track names to midi notes.
       Returns scheduled-fn, which can be stopped with stop."
       (let [step-ms (beat-ms 1/4 bpm)]
-        (reset! sequencer (every step-ms play-current-step atat-pool))))
+        (every step-ms #(play-and-advance patt notemap sink) atat-pool)))
 
-    (defn stop-sequence [] (stop @sequencer))
+    (def sequencer (do (reset! current-step 0)
+                       (play-pattern! patt 154 notemap sink)))
+    (stop sequencer)
 ;;
 ;;
 ;;     )
