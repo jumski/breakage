a8a03136b2ff928a248ff9bb4320a1f3e9c3b9e3
Wojtek Majewski <jumski@gmail.com>
2015-05-18 11:45:04 +0200

    Wip

--- src/jumski/breakage/tests/break_1.clj
+++ src/jumski/breakage/tests/break_1.clj
@@ -3,44 +3,30 @@
   (:use [jumski.breakage.mindstorm :only [defpattern patterns]])
   (:use [jumski.breakage.tests.akai-s2000 :as akai]))
 
-(def ch0 (akai/make-player "USB" 0))
-(def ch1 (akai/make-player "USB" 1))
-
-(def pattern (atom {0 {:midi-channel 0}
-         }))
-
 (comment
   (s/restart-sequencing :intro 154 ch0)
   (s/stop-sequencing :intro)
   )
 
-; SOURCE
-  ;;         . . . . . . . .
-  ;;         . . . . . . . .
+(def ch0 (akai/make-player "USB" 0))
+(def ch1 (akai/make-player "USB" 1))
 
-( defpattern :intro
+(defpattern :intro
   :kick1 8 .)
 
 (defpattern :intro
-  :kick5     8 . 3 . 8 . 4 .
-  :snare1    . . . . 5 . . .
-             . . . . . . . .
-  :snare7    . . . . . . . .
-             . 9 . . 9 . . 9
-             . . . . . . . .
-             . 9 . . . . . .
-  :snare8    . 9 . . 9 . . 9
-             . . . . . . . .
-             . 9 . . . . . .
-             . . . . . . . .
+  :kick5     6 . 3 . 8 . 4 .
+  :snare1    . . . . 5 . . .   . . . . . . . .
+  :snare7    . . . . . . . .   . 9 . . 9 . . 9
+             . . . . . . . .   . 9 . . . . . .
+  :snare8    . 9 . . 9 . . 9   . . . . . . . .
+             . 9 . . . . . .   . . . . . . . .
              ;; . . . . . . . .
              ;; . 9 . . . . . .
              ;; . . . . . . . .
              ;; . 9 . . . . . .
   )
 
-(defpattern :intro
-  :kick1  9 . . 9 9 . . .
   :kick1  9 . 3 9 . . 9 .
 
   :snare1 . . . . . 9 . .
@@ -54,7 +40,7 @@
   :chat1  2 . 4 . . 3
   :chat3  . . . . 4 . . .
   ;; :chat3  . 2 .
-  ;; :chat1  . 4
+  ;; :chat2  . 4
   :kick6
           4 4 4 4 4 . . .
           . . . . . . . .
