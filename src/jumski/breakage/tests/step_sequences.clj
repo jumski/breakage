c8fe3401d287dbbd28f74a688ae6696c4038bb0e
Wojtek Majewski <jumski@gmail.com>
2015-05-20 02:48:22 +0200

    Wip

--- src/jumski/breakage/tests/step_sequences.clj
+++ src/jumski/breakage/tests/step_sequences.clj
@@ -1,23 +1,31 @@
 (ns jumski.breakage.tests.step-sequences
-  (:use [jumski.breakage.tests.protocols :only [StepSequence]]))
+  (:use [jumski.breakage.tests.protocols :only [StepSequence]])
+  (:use [overtone.midi :only [note]]))
+
+;; (comment
+;;   {:time 0
+;;    :pitch 67
+;;    :duration 2
+;;    :part :melody}
+;; )
 
 (defrecord Patch [steps]
   StepSequence
-  (notes-for-step [step]
+  (notes-for-step [step tname->note]
     (for [[tname steps] (:steps this)
           :let [velo (nth (cycle steps) step)]
           :when (not (nil? velo))
           :let [velo (* 12.7 (inc velo))]]
       [anote velo]))
   (notes-for-step [step]
-    (notes-for-step step 1))
-    ))
+    (notes-for-step step note)))
 
 (defrecord Pattern [tracks]
   StepSequence
   (notes-for-step [step]
-    (let [[tno trk] (:tracks this)]
-      )
-    ))
+    (for [[ch patch] (:tracks this)
+          :let [notes (notes-for-step patch step)]]
+      [ch notes])))
+
 
 (defrecord Chain [parts])
