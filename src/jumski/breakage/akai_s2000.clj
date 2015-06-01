4611696b7d2f853fe6b2a0fab619c3059653ba6b
Wojtek Majewski <jumski@gmail.com>
2015-06-01 19:34:15 +0200

    Add drop out samples to akais2000

--- src/jumski/breakage/akai_s2000.clj
+++ src/jumski/breakage/akai_s2000.clj
@@ -9,10 +9,11 @@
 
 (def ^:private labels-to-notes
   "Maps track name to note"
-  {:chat1 :c#3 :chat2 :d#3 :chat3 :f#3 :chat4 :g#3 :chat5 :a#3
-   :kick1 :e2 :kick2 :f2 :kick3 :g2 :kick4 :a2 :kick5 :b2 :kick6 :c3
+  {:chat1 :c#3 :chat2 :d#3 :chat3 :f#3 :chat4 :g#3 :chat5 :a#3 :chat-drop :c#4
+
+   :kick1 :e2 :kick2 :f2 :kick3 :g2 :kick4 :a2 :kick5 :b2 :kick6 :c3 :kick-drop :d3
    :snare1 :f3 :snare2 :g3 :snare3 :a3 :snare4 :b3
-   :snare5 :c4 :snare6 :d4 :snare7 :e4 :snare8 :f4})
+   :snare5 :c4 :snare6 :d4 :snare7 :e4 :snare8 :f4 :snare-drop :g4})
 
 (defn tname->note [tname]
   "Given track name, returns note number.
