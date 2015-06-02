4aab249d66b49dedfbae389588641c01a133b8fc
Wojtek Majewski <jumski@gmail.com>
2015-06-02 10:06:38 +0200

    Wip

--- src/jumski/breakage/break_3.clj
+++ src/jumski/breakage/break_3.clj
@@ -32,6 +32,7 @@
 (def midimap { 1 :kicks1   2 :hats2   3 :snares2   4 :hard-snare                       })
 (def midimap { 1 :kicks1   2 :hats2   3 :snares2   4 :hard-snare   7 :snare-pitch      })
 (def midimap { 1 :kicks1   2 :hats2   3 :snares2   4 :hard-snare   7 :snare-pitch-fast })
+(def midimap { 4 :hard-snare})
 
 
 (defpatch :snare-pitch
@@ -53,14 +54,14 @@
   )
 
 (defpatch :break1
-  :kick-drop    6 . . .  +  . 6 . + .  .  . 6 .  .  .
-  :snare-drop   + . . . 5.6 . . . + . 5.6 . + .  .  .
-                + . . . 5.6 . . . + . 5.6 . + .  .  .
-                + . . . 5.6 . . . + . 5.6 . + . 5.6 .
+  :kick-drop    5 . . .  +  . 5 . + .  .  . 5 .  .  .
+  :snare-drop   + . . . 4.6 . . . + . 4.6 . + .  .  .
+                + . . . 4.6 . . . + . 4.6 . + .  .  .
+                + . . . 4.6 . . . + . 4.6 . + . 4.6 .
   )
 
 (defpatch :hats1
-  :chat-drop    + . 4 . + . 4 . 4 . . . . . 4 .
+  :chat-drop    + . 3 . + . 3 . 3 . . . . . 3 .
   )
 
 (defpatch :hats2
@@ -69,7 +70,6 @@
   )
 
 (defpatch :snares1
-  :c0           + . . . + . . . + . . . + . . .
   :snare6       . . . . . 4
   :snare5       . . 4 . . .
   :snare1       + . . . + . . . + . . . + . . .
@@ -90,9 +90,6 @@
   )
 
 (defpatch :hard-snare
-  :snare8 (repeat 48 nil)      + . . . + . . . 6 . . . + . . .
-  :kick6 (repeat 48 nil)       + . . . + . . . 6 . . . + . . .
-  ;; :kick1 (repeat 96 nil)      + . . . + . . . 8 . . . + . . .
-  ;; :snare2 (repeat 96 nil)      + . . . + . . . 5 . . . + . . .
-  ;; :snare3 (repeat 96 nil)      + . . . + . . . 5 . . . + . . .
+  :snare8 (repeat 48 nil)      + . . . + . . . . 6 . . + . . .
+  :snare7 (repeat 48 nil)      + . . . + . . . . 6 . . + . . .
   )
